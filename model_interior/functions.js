f8c7c0d0c9fc8f74458bb682b21ecac9bf307655
Dymitr-Szlykow <62315853+Dymitr-Szlykow@users.noreply.github.com>
2022-01-27 05:31:59 +0300

    к занятию 7

---
+++ model_interior/functions.js
@@ -0,0 +1,13 @@
+﻿function transitionsON() {
+	document.getElementById("searchInput").classList.remove('preload');
+	document.getElementById("searchButton").classList.remove('preload');
+}
+
+function randomLesserThan(cieling) {
+	return Math.floor(Math.random() * (cieling));
+}
+
+function swapNumberOfPictures(upTo) {
+	for(var i = 0;  i < upTo;  i++)
+		document.getElementById("item" + i).setAttribute('src', pictures[randomLesserThan(pictures.length)]);
+}
